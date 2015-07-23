fd87c82696ffd94e1c87a8f4f3014da114dd1c64
David Hayes <david@blackbricksoftware.com>
2015-07-23 18:15:22 -0500

    work on and test functionality

--- defaultmailtokens.php
+++ defaultmailtokens.php
@@ -136,3 +136,173 @@ function defaultmailtokens_civicrm_preProcess($formName, &$form) {
 }
 
 */
+
+
+/**
+ * Implements hook_civicrm_tokenValues().
+ * // found https://civicrm.org/blogs/colemanw/create-your-own-tokens-fun-and-profit
+*/
+//~ function defaultmailtokens_civicrm_tokenValues(&$values, $cids, $job = null, $tokens = array(), $context = null) {
+	//~ 
+	//~ //echo 'values:\n',print_r($values,true),"\ncids\n",print_r($cids,true),"\njob\n",print_r($job,true),"\ntokens\n",print_r($tokens,true),"\ncontext\n",print_r($context,true),"\n";
+	//~ 
+	//~ if (!empty($values)) {
+		//~ if (is_array($cids)) {
+			//~ /* values looks like
+			 //~ Array
+				//~ (
+					//~ [250331] => Array
+						//~ (
+							//~ [contact_id] => 250331
+							//~ [display_name] => John Smith
+							//~ [preferred_mail_format] => 
+							//~ [hash] => 56ca33ecaefbe170ae155660cf1812c6
+							//~ [first_name] => John
+							//~ [is_deceased] => 0
+							//~ [email_id] => 1235
+							//~ [on_hold] => 0
+						//~ )
+						//~ ... more records with pertinanat tokens ...
+				//~ )
+			//~ */
+			//~ foreach ($cids as $cid) {
+				//~ if (empty($values[$cid])) {
+					//~ // for some reason we werent passed a contact
+					 //~ $contact = civicrm_api3('Contact','get',array(
+						//~ 'contact_id' => $cid,
+					//~ ));
+					//~ if (!empty($contact['is_error'])) continue;
+					//~ $values[$cid] = array(
+						//~ 'contact_id' => $cid,
+						//~ 'display_name' => $contact['values'][$cid]['display_name'],
+						//~ 'preferred_mail_format' => $contact['values'][$cid]['preferred_mail_format'],
+						//~ 'is_deceased' => $contact['values'][$cid]['is_deceased'],
+						//~ 'email_id' => $contact['values'][$cid]['email_id'],
+						//~ 'on_hold' => $contact['values'][$cid]['on_hold'],						
+					//~ );
+				//~ }
+				//~ $used = array_key_exists('first_name',$values[$cid])||!empty($tokens['contact']['first_name']);
+				//~ if ($used&&empty($values[$cid]['first_name'])) $values[$cid]['first_name'] = 'Friend';
+			//~ }
+		//~ } else {
+			//~ /* we were passes a single contact record here */
+		//~ }
+	//~ }
+//~ 
+	//~ //echo 'values:\n',print_r($values,true),"\ncids\n",print_r($cids,true),"\njob\n",print_r($job,true),"\ntokens\n",print_r($tokens,true),"\ncontext\n",print_r($context,true),"\n"; exit;
+//~ }
+
+// https://civicrm.org/blogs/civicrm-team/civimail-how-add-default-values-empty-tokens?q=blogs/civicrm-team/civimail-how-add-default-values-empty-tokens
+function defaultmailtokens_civicrm_tokenValues( &$values, &$contactIDs, $job = null, $tokens = array(), $context = null) {
+    if ( is_array( $contactIDs ) ) {
+        $single = false;
+    } else {
+        $contactIDs = array( $contactIDs );
+        $single = true;
+    }
+
+    // lets assume we want default values for the below tokens
+    $defaults = array( 
+		'first_name'  => 'Friend',
+		//~ 'last_name'   => 'DEFAULT LAST',
+		//~ 'middle_name' => 'DEFAULT MIDDLE',
+		//~ 'home_URL'    => 'HOME URL'
+	);
+
+    foreach ( $contactIDs as $cid ) {
+        if ( $single ) {
+            $value =& $values;
+        } else {
+            $value =& $values[$cid];
+        }
+
+        foreach ( $defaults as $k => $v ) {
+            if ( ! isset( $value[$k] ) || empty( $value[$k] ) ) {
+                $value[$k] = $v;
+            }
+        }
+    }
+}
+
+/*
+Array
+(
+    [is_error] => 0
+    [version] => 3
+    [count] => 1
+    [id] => 251528
+    [values] => Array
+        (
+            [251528] => Array
+                (
+                    [contact_id] => 251528
+                    [contact_type] => Individual
+                    [contact_sub_type] => 
+                    [sort_name] => Knight
+                    [display_name] => Knight
+                    [do_not_email] => 0
+                    [do_not_phone] => 0
+                    [do_not_mail] => 0
+                    [do_not_sms] => 0
+                    [do_not_trade] => 0
+                    [is_opt_out] => 0
+                    [legal_identifier] => 
+                    [external_identifier] => 
+                    [nick_name] => 
+                    [legal_name] => 
+                    [image_URL] => 
+                    [preferred_communication_method] => 
+                    [preferred_language] => en_US
+                    [preferred_mail_format] => Both
+                    [first_name] => 
+                    [middle_name] => 
+                    [last_name] => Knight
+                    [prefix_id] => 
+                    [suffix_id] => 
+                    [formal_title] => 
+                    [communication_style_id] => 1
+                    [job_title] => 
+                    [gender_id] => 
+                    [birth_date] => 
+                    [is_deceased] => 0
+                    [deceased_date] => 
+                    [household_name] => 
+                    [organization_name] => 
+                    [sic_code] => 
+                    [contact_is_deleted] => 0
+                    [current_employer] => 
+                    [address_id] => 249321
+                    [street_address] => 
+                    [supplemental_address_1] => 
+                    [supplemental_address_2] => 
+                    [city] => 
+                    [postal_code_suffix] => 
+                    [postal_code] => 
+                    [geo_code_1] => 36.778261
+                    [geo_code_2] => -119.4179324
+                    [state_province_id] => 1004
+                    [country_id] => 1228
+                    [phone_id] => 
+                    [phone_type_id] => 
+                    [phone] => 
+                    [email_id] => 89531
+                    [email] => phillipkknight+jesus@gmail.com
+                    [on_hold] => 0
+                    [im_id] => 
+                    [provider_id] => 
+                    [im] => 
+                    [worldregion_id] => 2
+                    [world_region] => America South, Central, North and Caribbean
+                    [individual_prefix] => 
+                    [individual_suffix] => 
+                    [communication_style] => Formal
+                    [gender] => 
+                    [state_province_name] => CA
+                    [state_province] => CA
+                    [country] => United States
+                    [id] => 251528
+                )
+
+        )
+)
+*/
